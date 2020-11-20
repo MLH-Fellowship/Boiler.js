93b6b3bbc20f30f7d959976071adcd1e46b8c0c2
David Knox <dakdave26@gmail.com>
2020-11-20 16:49:21 -0500

    Integrated React into the project

--- main.js
+++ main.js
@@ -7,13 +7,13 @@ function createWindow () {
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
-    webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
+    // webPreferences: {
+    //   preload: path.join(__dirname, 'preload.js')
+    // }
   })
 
   // and load the index.html of the app.
-  mainWindow.loadFile('index.html')
+  mainWindow.loadFile('./build/index.html')
 
   // Open the DevTools.
   // mainWindow.webContents.openDevTools()
