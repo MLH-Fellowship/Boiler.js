441872cbe2cd52aa812f2e6fe4148cb40024c5d8
ZzRanger <calebmarklim@gmail.com>
2020-11-24 14:54:52 -0600

    download files

--- backend-local/download_files.js
+++ backend-local/download_files.js
@@ -11,4 +11,16 @@ function get_git_repo(url, dir) {
   });
 }
 
+// usage: run_commands(path.join(os.homedir(), 'Boilers', Backend-Demo), ["cd frontend", "npm i", "cd .. ", "cd backend", "npm i"])
+function run_commands(dirPath, commands){
+  let last = commands.pop();
+  let commandString = commands.map((val) => val + " &&").join(' ').concat(` ${last}`);
+  console.log(commandString);
+  exec(`${commandString}`, {cwd: dirPath}, function(err, stdout, stderr){
+      console.log(stdout, err, stderr);
+    });
+};
+
 module.exports = get_git_repo;
+
+run_commands(path.join(os.homedir(), 'Boilers', 'Backend-Demo'), ["cd frontend", "npm i", "cd .. ", "cd backend", "npm i"]);
