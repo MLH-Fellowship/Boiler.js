7521946b77f5b19ce67956fdc649c1787449096c
ZzRanger <calebmarklim@gmail.com>
2020-11-24 16:41:07 -0600

    works but meh

--- backend-local/download_files.js
+++ backend-local/download_files.js
@@ -4,34 +4,40 @@ const exec = require("child_process").exec;
 const execSync = require("child_process").execSync;
 
 // usage: get_git_repo("https://github.com/KohinaTheCat/Backend-Demo", "C:\\Users\\Clara\\Desktop\\MLH");
-async function get_git_repo(url, dir) {
+function get_git_repo(url, dir, commands) {
   const folderName = path.basename(url, path.extname(url));
   const fullPath = path.join(dir, folderName);
-  exec(`git clone ${url}.git "${fullPath}"`, function (err, stdout, stderr) {
+  exec(`git clone ${url}.git "${fullPath}"`, {
+    windowsHide: false
+  }, function (err, stdout, stderr) {
     console.log(stdout, err, stderr);  
-  });
+    run_commands(fullPath, commands);
+  })
   console.log(fullPath);
   return fullPath; 
-}
+};
 
 // usage: run_commands(path.join(os.homedir(), 'Boilers', 'Backend-Demo'), ["cd frontend", "npm i", "cd .. ", "cd backend", "npm i"]);
-async function run_commands(dirPath, commands){
-  console.log("Run");
+function run_commands(dirPath, commands){
+  console.log("Run", dirPath);
   let last = commands.pop();
   let commandString = commands.map((val) => val + " &&").join(' ').concat(` ${last}`);
-  console.log(commandString);
-  exec(`${commandString}`, {cwd: dirPath}, function(err, stdout, stderr){
+  console.log("String", commandString);
+  exec(`${commandString}`, {
+    cwd: dirPath,    
+    windowsHide: false
+  }, function(err, stdout, stderr){
       console.log(stdout, err, stderr);
     });
 };
+run_commands(path.join(os.homedir(), 'Boilers', 'Backend-Demo'), ["cd frontend", "npm i", "cd .. ", "cd backend", "npm i"]);
 
+/*
 // returns a promise
 async function wrapperFunc(res, boilerPath) {
   try {
       let r1 = await get_git_repo(res.repo, boilerPath);
-      if (r1 == null) {
-        let r2 = await run_commands(r1, res.commands);
-      }
+      let r2 = await run_commands(r1, res.commands);
       
       // now process r2
       return "Successful";     // this will be the resolved value of the returned promise
@@ -46,8 +52,8 @@ wrapperFunc().then(result => {
 }).catch(err => {
   // got error
 });
-
-module.exports = wrapperFunc
+*/
+module.exports = get_git_repo
 
 /*
 {
